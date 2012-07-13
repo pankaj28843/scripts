f2e6d79aa929cc0d9277c36efaf0957e6b38c820
Pankaj Singh <ps.jinx@gmail.com>
2012-07-13 17:47:49 +0530

    bug fix for mac

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 mkdir -p ~/bin
 cwd="$(pwd)"
 
-rm --force ~/bin/mcurl-patched && ln -s $cwd/mcurl ~/bin/mcurl-patched
-rm --force ~/bin/youtube-dl-patched && ln -s $cwd/youtube-dl ~/bin/youtube-dl-patched
-rm --force ~/bin/parse_youtube_urls.py && ln -s $cwd/parse_youtube_urls.py ~/bin/parse_youtube_urls.py
-rm --force ~/bin/yd && ln -s $cwd/youtube_downloader.sh ~/bin/yd
-rm --force ~/bin/ymd && ln -s $cwd/youtube_multi_downloader.sh ~/bin/ymd
-rm --force ~/bin/ywatch && ln -s $cwd/youtube_watcher.sh ~/bin/ywatch
-rm --force ~/bin/ywatch && ln -s $cwd/youtube_mp3_download.sh ~/bin/ymp3
+rm -f ~/bin/mcurl-patched && ln -s $cwd/mcurl ~/bin/mcurl-patched
+rm -f ~/bin/youtube-dl-patched && ln -s $cwd/youtube-dl ~/bin/youtube-dl-patched
+rm -f ~/bin/parse_youtube_urls.py && ln -s $cwd/parse_youtube_urls.py ~/bin/parse_youtube_urls.py
+rm -f ~/bin/yd && ln -s $cwd/youtube_downloader.sh ~/bin/yd
+rm -f ~/bin/ymd && ln -s $cwd/youtube_multi_downloader.sh ~/bin/ymd
+rm -f ~/bin/ywatch && ln -s $cwd/youtube_watcher.sh ~/bin/ywatch
+rm -f ~/bin/ywatch && ln -s $cwd/youtube_mp3_download.sh ~/bin/ymp3
