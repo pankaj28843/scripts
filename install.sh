2d4a47821a1399821d84339e2cc9fcf09d608249
Pankaj Singh <contact@iampankaj.in>
2012-11-07 14:54:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 mkdir -p ~/bin
 cwd="$(pwd)"
 
-rm -f ~/bin/mcurl-patched && ln -s $cwd/mcurl ~/bin/mcurl-patched
-rm -f ~/bin/youtube-dl-patched && ln -s $cwd/youtube-dl ~/bin/youtube-dl-patched
-rm -f ~/bin/parse_youtube_urls.py && ln -s $cwd/parse_youtube_urls.py ~/bin/parse_youtube_urls.py
-rm -f ~/bin/yd && ln -s $cwd/youtube_downloader.sh ~/bin/yd
-rm -f ~/bin/ymd && ln -s $cwd/youtube_multi_downloader.sh ~/bin/ymd
-rm -f ~/bin/ywatch && ln -s $cwd/youtube_watcher.sh ~/bin/ywatch
-rm -f ~/bin/ywatch && ln -s $cwd/youtube_mp3_download.sh ~/bin/ymp3
+ln -sf $cwd/mcurl ~/bin/mcurl-patched
+ln -sf $cwd/youtube-dl ~/bin/youtube-dl-patched
+ln -sf $cwd/parse_youtube_urls.py ~/bin/parse_youtube_urls.py
+ln -sf $cwd/youtube_downloader.sh ~/bin/yd
+ln -sf $cwd/youtube_multi_downloader.sh ~/bin/ymd
+ln -sf $cwd/youtube_watcher.sh ~/bin/ywatch
+ln -sf $cwd/youtube_mp3_download.sh ~/bin/ymp3
