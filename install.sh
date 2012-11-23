3589a4dc85ec39836e9d0d3b1c897c5ec9c0c0a8
Pankaj Singh <contact@iampankaj.in>
2012-11-23 12:09:07 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 git submodule init
-
 git submodule update
 
 mkdir -p ~/bin
@@ -13,4 +12,4 @@ cp -f $cwd/parse_youtube_urls.py ~/bin/parse_youtube_urls.py
 cp -f $cwd/youtube_downloader.sh ~/bin/yd
 ln -f $cwd/youtube_multi_downloader.sh ~/bin/ymd
 cp -f $cwd/youtube_watcher.sh ~/bin/ywatch
-cp -f $cwd/youtube_mp3_download.sh ~/bin/ymp3
+cp -f $cwd/youtube_mp3_downloader.sh ~/bin/ymp3
