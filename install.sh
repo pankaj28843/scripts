9c7fe95491a765fff2cecc109d37c0331bc13456
Pankaj Singh <contact@iampankaj.in>
2012-11-23 12:09:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ cp -f $cwd/mcurl ~/bin/mcurl-patched
 cp -f $cwd/youtube-dl/youtube-dl ~/bin/youtube-dl-patched
 cp -f $cwd/parse_youtube_urls.py ~/bin/parse_youtube_urls.py
 cp -f $cwd/youtube_downloader.sh ~/bin/yd
-ln -f $cwd/youtube_multi_downloader.sh ~/bin/ymd
+cp -f $cwd/youtube_multi_downloader.sh ~/bin/ymd
 cp -f $cwd/youtube_watcher.sh ~/bin/ywatch
 cp -f $cwd/youtube_mp3_downloader.sh ~/bin/ymp3
