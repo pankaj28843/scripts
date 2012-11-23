42c01f1926be03376c8f24aa7afcf5ad261f4857
Pankaj Singh <contact@iampankaj.in>
2012-11-23 11:58:17 +0530

    Update youtube_downloader.sh

--- youtube_downloader.sh
+++ youtube_downloader.sh
@@ -10,8 +10,8 @@ else
     dummy_url=$(youtube-dl-patched -g  "$1");
     filesize=$(youtube-dl-patched --get-filesize "$1")
 echo $filesize;
-    mcurl-patched -p 50 --filesize $filesize -o "$parsed_name" "$dummy_url" &
-    #wget -c --no-verbose "$dummy_url" -O "$parsed_name"&
+    #mcurl-patched -p 50 --filesize $filesize -o "$parsed_name" "$dummy_url" &
+    wget -c --verbose "$dummy_url" -O "$parsed_name"&
     #aria2c --summary-interval=0 show-console-readout=false -c -m0 -s10 $(echo dummy_url) -o "$parsed_name"&
     #aria2c  -c -m0 -s10 -o "$parsed_name" $dummy_url&
 fi
